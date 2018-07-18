929d8a5492c1949204c07856784b14e94ad9e317
Lucas Teles <lucasteles22@gmail.com>
2018-07-18 13:01:56 -0300

    Start project

---
+++ Models/User.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace UserRegistration.Models
+{
+    public class User : IdentityUser
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Document { get; set; }
+    }
+}

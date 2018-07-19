13822520e91892456f3f8d71d80493af59a677ea
Lucas Teles <lucasteles22@gmail.com>
2018-07-19 00:56:18 -0300

    Add Crud to User model

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
@@ -13,5 +13,6 @@ namespace UserRegistration.Data
             : base(options)
         {
         }
+        public DbSet<User> Users { get; set; }
     }
 }

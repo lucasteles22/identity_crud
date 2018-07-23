5a49c5b6a7fdbc52fe1248cd444ac61be4e6b24b
Lucas Teles <lucasteles22@gmail.com>
2018-07-23 17:05:27 -0300

    Change home interface

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,11 +1,9 @@
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using UserRegistration.Data;
 using UserRegistration.Models;
@@ -13,6 +11,7 @@ using UserRegistration.ViewModels;
 
 namespace UserRegistration.Controllers
 {
+    [Authorize]
     public class UsersController : Controller
     {
         private readonly ApplicationDbContext _context;
